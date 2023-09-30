c1363ce5898e77d58d64db5f40eb877340e7e4a8
lmendrick <128844788+lmendrick@users.noreply.github.com>
2023-09-30 11:37:43 -0400

    Added basic GameObject class

---
+++ game/TwelveMage/TwelveMage/GameObject.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * Lucas Mendrick
+ * Twelve-Mage
+ * This class serves as a base for all future game objects such as
+ * Player, Enemies, and Projectiles.
+ * No known issues.
+ */
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+public abstract class GameObject
+{
+	protected Rectangle position;
+	protected Texture2D texture;
+	public Rectangle Position {  get { return position; } }
+
+	protected GameObject(Rectangle position, Texture2D texture)
+	{
+		this.position = position;
+		this.texture = texture;
+	}
+
+	// Luke
+	// Purpose: Use a SpriteBatch obejct to draw the GameObject
+	// Params: spriteBatch - The SpriteBatch object passed in from the main Draw()
+	// Restrictions: Color is always white. May want to add a color field in the future.
+	public virtual void Draw(SpriteBatch spriteBatch)
+	{
+		spriteBatch.Draw(texture, position, Color.White);
+	}
+
+	// Luke
+	// Purpose: Implemented by child classes so that they can update themselves
+	// Params: gameTime - GameTime object passed in from the main Update() 
+	public abstract void Update(GameTime gameTime);
+}
