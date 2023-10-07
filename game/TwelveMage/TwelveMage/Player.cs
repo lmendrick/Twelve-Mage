4c72b4fe7c4d273d988cd393ba4f9d2915a3324a
lmendrick <128844788+lmendrick@users.noreply.github.com>
2023-10-07 17:37:34 -0400

    Added basic input/movement to Player and some code for drawing animations (unfinished)

--- game/TwelveMage/TwelveMage/Player.cs
+++ game/TwelveMage/TwelveMage/Player.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,22 +26,91 @@ namespace TwelveMage
         //private int _health;
         //private int _ammo; // Ammunition count (Not yet implemented)
 
-            // PROPERTIES
+        // PROPERTIES
         //public int Health { get { return _health; } }
 
-            // METHODS
-        public override void Update(GameTime gameTime) { } // Necessary for inheriting from GameObject
+
+        // Texture and drawing
+        Texture2D spriteSheet;  // The single image with all of the animation frames
+
+        // Animation
+        int frame;              // The current animation frame
+        double timeCounter;     // The amount of time that has passed
+        double fps;             // The speed of the animation
+        double timePerFrame;    // The amount of time (in fractional seconds) per frame
+
+        // Constants for "source" rectangle (inside the image)
+        const int WalkFrameCount = 3;       // The number of frames in the animation
+        const int MarioRectOffsetY = 116;   // How far down in the image are the frames?
+        const int MarioRectHeight = 72;     // The height of a single frame
+        const int MarioRectWidth = 44;      // The width of a single frame
+
+        // CONSTRUCTORS
+        // Luke: Added health inherited from GameObject
+        public Player(Rectangle position, Texture2D texture, int health) : base(position, texture, health)
+        {
+            this.spriteSheet = texture;
+
+            // Initialize
+            fps = 10.0;                     // Will cycle through 10 walk frames per second
+            timePerFrame = 1.0 / fps;       // Time per frame = amount of time in a single walk image
+        }
+
+        // METHODS
+        public override void Update(GameTime gameTime) // Necessary for inheriting from GameObject
+        {
+            KeyboardState kbState = Keyboard.GetState();
+
+            // Luke: Movement using WASD, probably should use Vector2 in future with
+            // normalized values to have diagonals same speed
+            if (kbState.IsKeyDown(Keys.W))
+            {
+                position.Y -= 1;
+            }
+            if (kbState.IsKeyDown(Keys.S))
+            {
+                position.Y += 1;
+            }
+            if (kbState.IsKeyDown (Keys.D))
+            {
+                position.X += 1;
+            }
+            if (kbState.IsKeyDown (Keys.A))
+            {
+                position.X -= 1;
+            }
+
+        }
+
+        // Luke: Taken from PE - Mario Walking
+        public void UpdateAnimation(GameTime gameTime)
+        {
+            // Handle animation timing
+            // - Add to the time counter
+            // - Check if we have enough "time" to advance the frame
+
+            // How much time has passed?  
+            timeCounter += gameTime.ElapsedGameTime.TotalSeconds;
+
+            // If enough time has passed:
+            if (timeCounter >= timePerFrame)
+            {
+                frame += 1;                     // Adjust the frame to the next image
+
+                if (frame > WalkFrameCount)     // Check the bounds - have we reached the end of walk cycle?
+                    frame = 1;                  // Back to 1 (since 0 is the "standing" frame)
+
+                timeCounter -= timePerFrame;    // Remove the time we "used" - don't reset to 0
+                                                // This keeps the time passed 
+            }
+        }
+
         public override void Draw(SpriteBatch spriteBatch) { } // Necessary for inheriting from GameObject
         public Boolean CheckCollision(GameObject _gameObject) // Checks collision with a given GameObject
         {
             return false;
         }
 
-            // CONSTRUCTORS
-            // Luke: Added health inherited from GameObject
-        public Player(Rectangle position, Texture2D texture, int health) : base(position, texture, health)
-        {
-            
-        }
+        
     }
 }
