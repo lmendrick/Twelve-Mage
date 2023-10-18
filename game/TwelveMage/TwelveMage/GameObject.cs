e8a3cffdd7844ef8fdb76fb194bbaf0e4fc75847
Anthony Maldonado <ajm4559@g.rit.edu>
2023-10-18 16:13:02 -0400

    added a check for collisions and scrapped some projectile stuff gonna start from scratch and test it a different

--- game/TwelveMage/TwelveMage/GameObject.cs
+++ game/TwelveMage/TwelveMage/GameObject.cs
@@ -14,8 +14,10 @@ public abstract class GameObject
 	protected Rectangle position;
 	protected Texture2D texture;
 	protected int health;
-	public Rectangle Position {  get { return position; } }
+    protected bool isActive = true;
+    public Rectangle Position {  get { return position; } }
 	public int Health { get { return health; } set { health = value; } }
+	
 
 	protected GameObject(Rectangle position, Texture2D texture, int health)
 	{
@@ -24,11 +26,32 @@ public abstract class GameObject
 		this.health = health;
 	}
 
-	// Luke
-	// Purpose: Use a SpriteBatch obejct to draw the GameObject
-	// Params: spriteBatch - The SpriteBatch object passed in from the main Draw()
-	// Restrictions: Color is always white. May want to add a color field in the future.
-	public virtual void Draw(SpriteBatch spriteBatch)
+    //Anthony
+    //Check if game object collides with one another
+    public bool CheckCollision(GameObject check)
+    {
+        //if the object is inactive then nothing happens
+        if (isActive == false)
+        {
+            return false;
+        }
+        else
+        {
+            //but if its active check if they instersect and if they do return true
+            if (position.Intersects(check.Position))
+            {
+                isActive = false;
+                return true;
+            }
+            else { return false; }
+        }
+    }
+
+    // Luke
+    // Purpose: Use a SpriteBatch obejct to draw the GameObject
+    // Params: spriteBatch - The SpriteBatch object passed in from the main Draw()
+    // Restrictions: Color is always white. May want to add a color field in the future.
+    public virtual void Draw(SpriteBatch spriteBatch)
 	{
 		spriteBatch.Draw(texture, position, Color.White);
 	}
--- game/TwelveMage/TwelveMage/Projectile.cs
+++ game/TwelveMage/TwelveMage/Projectile.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,29 @@ using System.Threading.Tasks;
 
 namespace TwelveMage
 {
-    internal class Projectile
+    internal class Projectile : GameObject
     {
+
+        //fields
+        
+
+
+        //methods
+        //check if the collectable is colliding with the player
+      
+
+        public Projectile(Rectangle position, Texture2D texture, int health) : base(position, texture, health)
+        {
+            
+        }
+
+
+        public override void Update(GameTime gameTime) // Necessary for inheriting from GameObject
+        {
+            
+        }
+
+        public override void Draw(SpriteBatch spriteBatch) { } // Necessary for inheriting from GameObject
+
     }
 }
