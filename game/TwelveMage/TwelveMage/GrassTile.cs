ed4bb36247293dc183906fa6727b46c5d6ad5e72
Ethan Hall <erh8978@rit.edu>
2023-10-19 19:00:24 -0400

    Tile classes WIP

---
+++ game/TwelveMage/TwelveMage/GrassTile.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+    * Chloe Hall
+    * Twelve-Mage
+    * This class inherits from Tile, and is specifically for grass tiles
+    * No known issues.
+    */
+namespace TwelveMage
+{
+    internal class GrassTile : Tile
+    {
+        public GrassTile(Texture2D texture, Vector2 loc) : base(texture, loc)
+        {
+
+        }
+    }
+}
---
+++ game/TwelveMage/TwelveMage/Tile.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwelveMage
+{
+    /*
+    * Chloe Hall
+    * Twelve-Mage
+    * This abstract class is a base for background tiles, 
+    * such as grass, stones, flowers, etc.
+    * No known issues.
+    */
+    internal abstract class Tile
+    {
+        #region FIELDS
+        protected Texture2D texture; // Tile sprite
+        protected Vector2 loc; // Tile location
+        protected const int TextureScale = 32; // Scale of the tile's texture, in pixels/side
+        #endregion
+
+        #region PROPERTIES
+        protected Vector2 Loc
+        { 
+            get { return loc; }
+        }
+        #endregion
+
+        #region CONSTRUCTORS
+        protected Tile(Texture2D texture, Vector2 loc)
+        {
+            this.texture = texture;
+            this.loc = loc;
+        }
+        #endregion
+
+        #region METHODS
+        public void Draw(SpriteBatch _spriteBatch) // Draws this tile in the appropriate location.
+        {
+            // Cast the tile's float location to a square (Rectangle) with TextureScale length sides
+            Rectangle drawLocation = new Rectangle((int)loc.X, (int)loc.Y, TextureScale, TextureScale);
+            _spriteBatch.Draw(texture, drawLocation, Color.White); // Draw this tile
+        }
+        #endregion
+    }
+}
