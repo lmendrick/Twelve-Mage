7c0976cdbdfd8e3642019cf72fc914f829365223
lmendrick <128844788+lmendrick@users.noreply.github.com>
2023-11-28 20:34:36 -0500

    Started working on border flames
    - May be a better idea to make a BorderFlame object and then a BorderFlamesManager
    - Could then use flames in other parts of game if we wanted to

---
+++ game/TwelveMage/TwelveMage/BorderFlames.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+
+namespace TwelveMage
+{
+	internal class BorderFlames
+	{
+		private TextureLibrary textureLibrary;
+		private int windowWidth;
+		private int windowHeight;
+
+        // Animation
+        private int frame;              // The current animation frame
+        private double timeCounter;     // The amount of time that has passed
+        private double fps;             // The speed of the animation
+        private double timePerFrame;    // The amount of time (in fractional seconds) per frame
+
+        // Constants for "source" rectangle (inside the image)
+        const int FireFrameCount = 6;       // The number of frames in the animation
+        //const int WizardRectOffsetY = 36;   // How far down in the image are the frames?
+        //const int WizardRectOffsetX = 4;
+        const int FireRectHeight = 30;     // The height of a single frame
+        const int FireRectWidth = 34;      // The width of a single frame
+
+        private int numHorizSprites;
+        private int numVertSprites;
+
+        public BorderFlames(TextureLibrary textureLibrary, int windowWidth, int windowHeight)
+		{
+			this.textureLibrary = textureLibrary;
+			this.windowWidth = windowWidth;
+			this.windowHeight = windowHeight;
+            numHorizSprites = windowWidth / FireRectWidth;
+            numVertSprites = windowHeight / FireRectHeight;
+		}
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+    }
+}
